4abe2dde760c22bfe48e5a4ae18065f74de73a8c
Wolfcrafter321 <wolfcrafter321@gmail.com>
2024-12-01 22:20:11 +0900

    Initial Commit

---
+++ Scripts/RuntimeInit.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class RuntimeInit : MonoBehaviour
+{
+
+    [RuntimeInitializeOnLoadMethod( RuntimeInitializeLoadType.BeforeSceneLoad )]
+    public static void Init()
+    {
+        var obj =Instantiate(Resources.Load("Manager")) ;
+        DontDestroyOnLoad( obj );
+
+    }
+
+    private void Start()
+    {
+        Destroy(this.gameObject);
+    }
+}
