6dffbbb8ebb9653532c250dc5f7aec58d57be775
Wolfcrafter321 <Wolfcrafter321@gmail.com>
2024-12-14 17:03:36 +0900

    Add Init Folders Command

--- Scripts/Utils/Editor/InitWolfFolder.cs
+++ Scripts/Utils/Editor/InitWolfFolder.cs
@@ -11,8 +11,46 @@ public class InitWolfFolder : MonoBehaviour
 
     [MenuItem("Wolf/Init Wolf Workspace")]
     static void Test()
-    {
-        Debug.Log("Hi");
+    { // フォルダ構成のルート
+        string root = "Assets";
+
+        // 作成するフォルダ一覧
+        string[] folders = new string[]
+        {
+            "Art",
+            "Art/Materials",
+            "Art/VFX",
+            "Art/Models",
+            "Art/Characters",
+            "Art/Characters/Sample",
+            "Art/Characters/Sample/FBX",
+            "Scripts",
+            "Scripts/Gameplay",
+            "Scripts/Editor",
+            "Resources",
+            "Scenes",
+            "Settings",
+            "Audio",
+            "Audio/Music",
+            "Audio/SFX"
+        };
+
+
+        foreach (string folder in folders)
+        {
+            string folderPath = Path.Combine(root, folder);
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            else
+            {
+                Debug.Log($"Folder already exists: {folderPath}");
+            } 
+        }
+
+        AssetDatabase.Refresh();
     }
 }
 #endif
